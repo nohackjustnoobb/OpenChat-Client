1a1e816ca1dc541b30bbaad2b6eaacfb0c44cfdf
nohackjustnoobb <hey1987808049@gmail.com>
2021-09-26 12:48:36 +0800

    change the app name to openchat

--- ios/openchatTests/openchatTests.m
+++ ios/openchatTests/openchatTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface openchatclientTests : XCTestCase
+@interface openchatTests : XCTestCase
 
 @end
 
-@implementation openchatclientTests
+@implementation openchatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
